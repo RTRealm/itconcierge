e5cfc23c57528bf90fc7903faf7cd61757be3f96
Tomasz Gasienica-Szostak <casprovy.tgs@gmail.com>
2020-06-18 23:12:28 +0200

    work in progress

--- src/App.js
+++ src/App.js
@@ -5,174 +5,192 @@ class App extends Component {
 	render() {
 		return (
 			<div className='App'>
-				<body style={{ margin: 0, background: 'rgba(255, 255, 255, 1.0)' }}>
+				<div style={{ margin: 0, background: 'rgba(255, 255, 255, 1.0)' }}>
 					<input id='anPageName' name='page' type='hidden' value='website' />
-					<div class='website anima-word-break '>
+					<div className='website anima-word-break '>
 						<div style={{ width: 1440, height: 100, position: 'relative', margin: 'auto' }}>
-							<img alt='Image' src='./img/website-oval@2x.png' class='oval' />
-							<img alt='Image' src='./img/website-rectangle.png' class='rectangle' />
-							<div class='rectangle1'></div>
-							<img alt='Image' src='./img/website-rectangle-copy@2x.png' class='rectanglecopy' />
-							<img alt='Image' src='./img/website-combined-shape@2x.png' class='combinedshape' />
-							<div class='home'>
-								<span class='span1'>HOME</span>
+							<img alt='Image' src='./img/website-oval@2x.png' className='oval' />
+							<img alt='Image' src='./img/website-rectangle.png' className='rectangle' />
+							<div className='rectangle1'></div>
+							<img
+								alt='Image'
+								src='./img/website-rectangle-copy@2x.png'
+								className='rectanglecopy'
+							/>
+							<img
+								alt='Image'
+								src='./img/website-combined-shape@2x.png'
+								className='combinedshape'
+							/>
+							<div className='home'>
+								<span className='span1'>HOME</span>
 							</div>
-							<div class='about'>
-								<span class='span1'>ABOUT</span>
+							<div className='about'>
+								<span className='span1'>ABOUT</span>
 							</div>
-							<div class='services'>
-								<span class='span1'>SERVICES</span>
+							<div className='services'>
+								<span className='span1'>SERVICES</span>
 							</div>
-							<div class='portfolio'>
-								<span class='span1'>PORTFOLIO</span>
+							<div className='portfolio'>
+								<span className='span1'>PORTFOLIO</span>
 							</div>
-							<img alt='Image' src='/img/website-rectangle-1@2x.png' class='rectangle2' />
-							<div class='contactus'>CONTACT US</div>
-							<img alt='Image' src='/img/website-group.png' class='group' />
-							<div class='wecreatecontentth'>
+							<img alt='Image' src='/img/website-rectangle-1@2x.png' className='rectangle2' />
+							<div className='contactus'>CONTACT US</div>
+							<img alt='Image' src='/img/website-group.png' className='group' />
+							<div className='wecreatecontentth'>
 								We Create
 								<br />
 								Content that
 								<br />
 								ranks
 							</div>
-							<div class='aboutus'>About Us</div>
-							<div class='loremipsumdolorsi'>
+							<div className='aboutus'>About Us</div>
+							<div className='loremipsumdolorsi'>
 								4ITS focuses on technology outsourcing and IT solutions. We leverage on the team's
 								decades long experience in the areas of IT departments, coding as well as running
 								business organizations and projects.
 							</div>
-							<div class='rectangle3'></div>
-							<div class='ourvaluableservice'>Our Valuable Services</div>
-							<div class='rectangle4'></div>
-							<div class='rectanglecopy3'></div>
-							<div class='rectanglecopy4'></div>
-							<img alt='Image' src='./img/website-bitmap.png' class='bitmap' />
-							<div class='loremipsumdolorsi1'>
+							<div className='rectangle3'></div>
+							<div className='ourvaluableservice'>Our Valuable Services</div>
+							<div className='rectangle4'></div>
+							<div className='rectanglecopy3'></div>
+							<div className='rectanglecopy4'></div>
+							<img alt='Image' src='./img/website-bitmap.png' className='bitmap' />
+							<div className='loremipsumdolorsi1'>
 								Web dev dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
 								incididunt ut labore et dolore magna aliqua.
 							</div>
-							<img alt='Image' src='./img/website-bitmap-2@2x.png' class='bitmap2' />
-							<div class='loremipsumdolorsicopy'>
+							<img alt='Image' src='./img/website-bitmap-2@2x.png' className='bitmap2' />
+							<div className='loremipsumdolorsicopy'>
 								App dev ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
 								incididunt ut labore et dolore magna aliqua.
 							</div>
-							<img alt='Image' src='./img/website-bitmap-1.png' class='bitmap1' />
-							<div class='loremipsumdolorsicopy2'>
+							<img alt='Image' src='./img/website-bitmap-1.png' className='bitmap1' />
+							<div className='loremipsumdolorsicopy2'>
 								UX/UI ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
 								incididunt ut labore et dolore magna aliqua.
 							</div>
-							<div class='webdevelopment'>Web Development</div>
-							<div class='appdevelopment'>App Development</div>
-							<div class='uiuxdesign'>UI UX Design</div>
-							<img alt='Image' src='./img/website-rectangle-copy-2@2x.png' class='rectanglecopy5' />
-							<div class='group2'>
+							<div className='webdevelopment'>Web Development</div>
+							<div className='appdevelopment'>App Development</div>
+							<div className='uiuxdesign'>UI UX Design</div>
+							<img
+								alt='Image'
+								src='./img/website-rectangle-copy-2@2x.png'
+								className='rectanglecopy5'
+							/>
+							<div className='group2'>
 								<img
 									alt='Image'
 									src='./img/website-rectangle-copy-2@2x.png'
-									class='rectanglecopy6'
+									className='rectanglecopy6'
 								/>
-								<div class='readmorecopy'>READ MORE</div>
+								<div className='readmorecopy'>READ MORE</div>
 							</div>
-							<div class='group2copy'>
+							<div className='group2copy'>
 								<img
 									alt='Image'
 									src='./img/website-rectangle-copy-2@2x.png'
-									class='rectanglecopy6'
+									className='rectanglecopy6'
 								/>
-								<div class='seeallwork'>SEE ALL WORK</div>
+								<div className='seeallwork'>SEE ALL WORK</div>
 							</div>
-							<img alt='Image' src='./img/website-rectangle-copy-2@2x.png' class='rectanglecopy7' />
-							<div class='readmore'>READ MORE</div>
-							<div class='readmorecopy2'>READ MORE</div>
-							<img alt='Image' src='./img/website-oval-1@2x.png' class='oval1' />
-							<img alt='Image' src='./img/website-path-2@2x.png' class='path2' />
-							<div class='ourwork'>Our Work</div>
-							<div class='brandname'>Brand Name</div>
-							<div class='brandnamecopy2'>Brand Name</div>
-							<div class='brandnamecopy'>Brand Name</div>
-							<div class='brandnamecopy3'>Brand Name</div>
-							<div class='iosandroidweb'>iOS | ANDROID | WEB DEVELOPMENT</div>
-							<div class='iosandroidwebcopy2'>iOS | ANDROID | WEB DEVELOPMENT</div>
-							<div class='iosandroidwebcopy'>iOS | ANDROID | WEB DEVELOPMENT</div>
-							<div class='iosandroidwebcopy3'>iOS | ANDROID | WEB DEVELOPMENT</div>
-							<div class='loremipsumdolorsicopy3'>
+							<img
+								alt='Image'
+								src='./img/website-rectangle-copy-2@2x.png'
+								className='rectanglecopy7'
+							/>
+							<div className='readmore'>READ MORE</div>
+							<div className='readmorecopy2'>READ MORE</div>
+							<img alt='Image' src='./img/website-oval-1@2x.png' className='oval1' />
+							<img alt='Image' src='./img/website-path-2@2x.png' className='path2' />
+							<div className='ourwork'>Our Work</div>
+							<div className='brandname'>Brand Name</div>
+							<div className='brandnamecopy2'>Brand Name</div>
+							<div className='brandnamecopy'>Brand Name</div>
+							<div className='brandnamecopy3'>Brand Name</div>
+							<div className='iosandroidweb'>iOS | ANDROID | WEB DEVELOPMENT</div>
+							<div className='iosandroidwebcopy2'>iOS | ANDROID | WEB DEVELOPMENT</div>
+							<div className='iosandroidwebcopy'>iOS | ANDROID | WEB DEVELOPMENT</div>
+							<div className='iosandroidwebcopy3'>iOS | ANDROID | WEB DEVELOPMENT</div>
+							<div className='loremipsumdolorsicopy3'>
 								App Dev r sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
 								labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
 								ullamco laboris nisi ut aliquip ex ea commodo consequat.
 							</div>
-							<div class='loremipsumdolorsicopy5'>
+							<div className='loremipsumdolorsicopy5'>
 								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
 								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
 								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 							</div>
-							<div class='loremipsumdolorsicopy4'>
+							<div className='loremipsumdolorsicopy4'>
 								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
 								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
 								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 							</div>
-							<div class='loremipsumdolorsicopy6'>
+							<div className='loremipsumdolorsicopy6'>
 								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
 								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
 								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 							</div>
-							<img alt='Image' src='./img/website-line-copy-2@2x.png' class='line' />
-							<img alt='Image' src='./img/website-line-copy-2@2x.png' class='linecopy' />
-							<img alt='Image' src='./img/website-line-copy-2@2x.png' class='linecopy2' />
-							<div class='bitmap3'>
-								<div class='mask'></div>
-								<img alt='Image' src='./img/website-bitmap-3.png' class='bitmap4' />
+							<img alt='Image' src='./img/website-line-copy-2@2x.png' className='line' />
+							<img alt='Image' src='./img/website-line-copy-2@2x.png' className='linecopy' />
+							<img alt='Image' src='./img/website-line-copy-2@2x.png' className='linecopy2' />
+							<div className='bitmap3'>
+								<div className='mask'></div>
+								<img alt='Image' src='./img/website-bitmap-3.png' className='bitmap4' />
 							</div>
-							<div class='bitmapcopy2'>
-								<div class='mask'></div>
-								<img alt='Image' src='./img/website-bitmap-4.png' class='bitmap4' />
+							<div className='bitmapcopy2'>
+								<div className='mask'></div>
+								<img alt='Image' src='./img/website-bitmap-4.png' className='bitmap4' />
 							</div>
-							<div class='bitmapcopy'>
-								<div class='mask'></div>
-								<img alt='Image' src='./img/website-bitmap-5.png' class='bitmap4' />
+							<div className='bitmapcopy'>
+								<div className='mask'></div>
+								<img alt='Image' src='./img/website-bitmap-5.png' className='bitmap4' />
 							</div>
-							<div class='bitmapcopy3'>
-								<div class='mask'></div>
-								<img alt='Image' src='./img/website-bitmap-6.png' class='bitmap4' />
+							<div className='bitmapcopy3'>
+								<div className='mask'></div>
+								<img alt='Image' src='./img/website-bitmap-6.png' className='bitmap4' />
 							</div>
-							<div class='form'>
-								<div class='button'>
-									<img alt='Image' src='./img/website-rectangle-3.png' class='rectangle31' />
-									<div class='sendamessage'>Send a Message</div>
+							<div className='form'>
+								<div className='button'>
+									<img alt='Image' src='./img/website-rectangle-3.png' className='rectangle31' />
+									<div className='sendamessage'>Send a Message</div>
 								</div>
-								<div class='message'>
-									<div class='editbox'>
-										<img alt='Image' src='./img/website-base.png' class='base' />
-										<div class='yourmessagestarts'>Your message starts with…</div>
+								<div className='message'>
+									<div className='editbox'>
+										<img alt='Image' src='./img/website-base.png' className='base' />
+										<div className='yourmessagestarts'>Your message starts with…</div>
 									</div>
-									<div class='message1'>Message</div>
+									<div className='message1'>Message</div>
 								</div>
-								<div class='subject'>
-									<div class='editbox'>
-										<img alt='Image' src='./img/website-base-2.png' class='base' />
-										<div class='questionaboutyour'>Question about your article</div>
+								<div className='subject'>
+									<div className='editbox'>
+										<img alt='Image' src='./img/website-base-2.png' className='base' />
+										<div className='questionaboutyour'>Question about your article</div>
 									</div>
-									<div class='subject1'>Subject</div>
+									<div className='subject1'>Subject</div>
 								</div>
-								<div class='email'>
-									<div class='editbox'>
-										<img alt='Image' src='./img/website-base-2.png' class='base' />
-										<div class='somethingwebsiteco'>something@website.com</div>
+								<div className='email'>
+									<div className='editbox'>
+										<img alt='Image' src='./img/website-base-2.png' className='base' />
+										<div className='somethingwebsiteco'>something@website.com</div>
 									</div>
-									<div class='youremailaddress'>Your Email Address</div>
+									<div className='youremailaddress'>Your Email Address</div>
 								</div>
 							</div>
-							<div class='questionscomments'>Questions, Comments, Concerns?</div>
-							<div class='weu2019dlovetohearfr'>
-								<span class='span1'>We’d love to </span>
-								<span class='span2'>hear</span>
-								<span class='span3'> from you!</span>
+							<div className='questionscomments'>Questions, Comments, Concerns?</div>
+							<div className='weu2019dlovetohearfr'>
+								<span className='span1'>We’d love to </span>
+								<span className='span2'>hear</span>
+								<span className='span3'> from you!</span>
 							</div>
-							<div class='rectangle5'></div>
-							<div class='a2020251allrightsre'>2020 © All rights reserved by Your company name</div>
+							<div className='rectangle5'></div>
+							<div className='a2020251allrightsre'>
+								2020 © All rights reserved by Your company name
+							</div>
 						</div>
 					</div>
-				</body>
+				</div>
 			</div>
 		);
 	}
