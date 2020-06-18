7cc0fa01279cb6f9247f5a37d0468d01b6e8e6bd
Tomasz Gasienica-Szostak <casprovy.tgs@gmail.com>
2020-06-18 22:09:00 +0200

    content copied from html

--- src/App.js
+++ src/App.js
@@ -2,15 +2,186 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>ItConcierge</p>
-			</header>
-		</div>
-	);
+class App extends React.Component {
+	render() {
+		return (
+			<div className='App'>
+				<body style={{ margin: 0, background: 'rgba(255, 255, 255, 1.0)' }}>
+					<input id='anPageName' name='page' type='hidden' value='website' />
+					<div class='website anima-word-break '>
+						<div style={{ width: 1440, height: 100, position: 'relative', margin: 'auto' }}>
+							<img alt='Image' src='./img/website-oval@2x.png' class='oval' />
+							<img alt='Image' src='./img/website-rectangle.png' class='rectangle' />
+							<div class='rectangle1'></div>
+							<img alt='Image' src='./img/website-rectangle-copy@2x.png' class='rectanglecopy' />
+							<img alt='Image' src='./img/website-combined-shape@2x.png' class='combinedshape' />
+							<div class='home'>
+								<span class='span1'>HOM</span>
+								<span class='span2'>E</span>
+							</div>
+							<div class='about'>
+								<span class='span1'>ABOU</span>
+								<span class='span2'>T</span>
+							</div>
+							<div class='services'>
+								<span class='span1'>SERVICE</span>
+								<span class='span2'>S</span>
+							</div>
+							<div class='portfolio'>
+								<span class='span1'>PORTFOLI</span>
+								<span class='span2'>O</span>
+							</div>
+							<img alt='Image' src='/img/website-rectangle-1@2x.png' class='rectangle2' />
+							<img alt='Image' src='./img/website-rectangle-copy-2@2x.png' class='rectanglecopy2' />
+							<div class='contactus'>CONTACT US</div>
+							<div class='seeourwork'>SEE OUR WORK</div>
+							<img alt='Image' src='/img/website-group.png' class='group' />
+							<div class='wecreatecontentth'>
+								We Create
+								<br />
+								Content that
+								<br />
+								ranks
+							</div>
+							<div class='aboutus'>About Us</div>
+							<div class='loremipsumdolorsi'>
+								About ussss d minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+								aliquip ex ea commodo consequat.
+							</div>
+							<div class='rectangle3'></div>
+							<div class='ourvaluableservice'>Our Valuable Services</div>
+							<div class='rectangle4'></div>
+							<div class='rectanglecopy3'></div>
+							<div class='rectanglecopy4'></div>
+							<img alt='Image' src='./img/website-bitmap.png' class='bitmap' />
+							<div class='loremipsumdolorsi1'>
+								Web dev dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+								incididunt ut labore et dolore magna aliqua.
+							</div>
+							<img alt='Image' src='./img/website-bitmap-2@2x.png' class='bitmap2' />
+							<div class='loremipsumdolorsicopy'>
+								App dev ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+								incididunt ut labore et dolore magna aliqua.
+							</div>
+							<img alt='Image' src='./img/website-bitmap-1.png' class='bitmap1' />
+							<div class='loremipsumdolorsicopy2'>
+								UX/UI ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+								incididunt ut labore et dolore magna aliqua.
+							</div>
+							<div class='webdevelopment'>Web Development</div>
+							<div class='appdevelopment'>App Development</div>
+							<div class='uiuxdesign'>UI UX Design</div>
+							<img alt='Image' src='./img/website-rectangle-copy-2@2x.png' class='rectanglecopy5' />
+							<div class='group2'>
+								<img
+									alt='Image'
+									src='./img/website-rectangle-copy-2@2x.png'
+									class='rectanglecopy6'
+								/>
+								<div class='readmorecopy'>READ MORE</div>
+							</div>
+							<div class='group2copy'>
+								<img
+									alt='Image'
+									src='./img/website-rectangle-copy-2@2x.png'
+									class='rectanglecopy6'
+								/>
+								<div class='seeallwork'>SEE ALL WORK</div>
+							</div>
+							<img alt='Image' src='./img/website-rectangle-copy-2@2x.png' class='rectanglecopy7' />
+							<div class='readmore'>READ MORE</div>
+							<div class='readmorecopy2'>READ MORE</div>
+							<img alt='Image' src='./img/website-oval-1@2x.png' class='oval1' />
+							<img alt='Image' src='./img/website-path-2@2x.png' class='path2' />
+							<div class='ourwork'>Our Work</div>
+							<div class='brandname'>Brand Name</div>
+							<div class='brandnamecopy2'>Brand Name</div>
+							<div class='brandnamecopy'>Brand Name</div>
+							<div class='brandnamecopy3'>Brand Name</div>
+							<div class='iosandroidweb'>iOS | ANDROID | WEB DEVELOPMENT</div>
+							<div class='iosandroidwebcopy2'>iOS | ANDROID | WEB DEVELOPMENT</div>
+							<div class='iosandroidwebcopy'>iOS | ANDROID | WEB DEVELOPMENT</div>
+							<div class='iosandroidwebcopy3'>iOS | ANDROID | WEB DEVELOPMENT</div>
+							<div class='loremipsumdolorsicopy3'>
+								App Dev r sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+								labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+								ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							</div>
+							<div class='loremipsumdolorsicopy5'>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							</div>
+							<div class='loremipsumdolorsicopy4'>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							</div>
+							<div class='loremipsumdolorsicopy6'>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							</div>
+							<img alt='Image' src='./img/website-line-copy-2@2x.png' class='line' />
+							<img alt='Image' src='./img/website-line-copy-2@2x.png' class='linecopy' />
+							<img alt='Image' src='./img/website-line-copy-2@2x.png' class='linecopy2' />
+							<div class='bitmap3'>
+								<div class='mask'></div>
+								<img alt='Image' src='./img/website-bitmap-3.png' class='bitmap4' />
+							</div>
+							<div class='bitmapcopy2'>
+								<div class='mask'></div>
+								<img alt='Image' src='./img/website-bitmap-4.png' class='bitmap4' />
+							</div>
+							<div class='bitmapcopy'>
+								<div class='mask'></div>
+								<img alt='Image' src='./img/website-bitmap-5.png' class='bitmap4' />
+							</div>
+							<div class='bitmapcopy3'>
+								<div class='mask'></div>
+								<img alt='Image' src='./img/website-bitmap-6.png' class='bitmap4' />
+							</div>
+							<div class='form'>
+								<div class='button'>
+									<img alt='Image' src='./img/website-rectangle-3.png' class='rectangle31' />
+									<div class='sendamessage'>Send a Message</div>
+								</div>
+								<div class='message'>
+									<div class='editbox'>
+										<img alt='Image' src='./img/website-base.png' class='base' />
+										<div class='yourmessagestarts'>Your message starts with…</div>
+									</div>
+									<div class='message1'>Message</div>
+								</div>
+								<div class='subject'>
+									<div class='editbox'>
+										<img alt='Image' src='./img/website-base-2.png' class='base' />
+										<div class='questionaboutyour'>Question about your article</div>
+									</div>
+									<div class='subject1'>Subject</div>
+								</div>
+								<div class='email'>
+									<div class='editbox'>
+										<img alt='Image' src='./img/website-base-2.png' class='base' />
+										<div class='somethingwebsiteco'>something@website.com</div>
+									</div>
+									<div class='youremailaddress'>Your Email Address</div>
+								</div>
+							</div>
+							<div class='questionscomments'>Questions, Comments, Concerns?</div>
+							<div class='weu2019dlovetohearfr'>
+								<span class='span1'>We’d love to </span>
+								<span class='span2'>hear</span>
+								<span class='span3'> from you!</span>
+							</div>
+							<div class='rectangle5'></div>
+							<div class='a2020251allrightsre'>2020 © All rights reserved by Your company name</div>
+						</div>
+					</div>
+				</body>
+			</div>
+		);
+	}
 }
 
 export default App;
