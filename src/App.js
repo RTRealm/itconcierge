28024ba9f3238d27213e0946a92e4f7cc26d6d96
Tomasz Gasienica-Szostak <casprovy.tgs@gmail.com>
2020-06-20 20:32:48 +0200

    working on layout from scratch

--- src/App.js
+++ src/App.js
@@ -1,198 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Service from './components/service/Service';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Service from './components/Service/Service';
 
 class App extends Component {
 	render() {
 		return (
-			<div className='App'>
-				<div style={{ margin: 0 }}>
-					<input id='anPageName' name='page' type='hidden' value='website' />
-					<div className='website anima-word-break '>
-						<div style={{ width: 1440, height: 100, position: 'relative', margin: 'auto' }}>
-							<img alt='Image' src='../src/img/website-oval@2x.png' className='oval' />
-							<img alt='Image' src='./img/website-rectangle.png' className='rectangle' />
-							<div className='rectangle1'></div>
-							<img
-								alt='Image'
-								src='./img/website-rectangle-copy@2x.png'
-								className='rectanglecopy'
-							/>
-							<img
-								alt='Image'
-								src='./img/website-combined-shape@2x.png'
-								className='combinedshape'
-							/>
-							<div className='home'>
-								<span className='span1'>HOME</span>
-							</div>
-							<div className='about'>
-								<span className='span1'>ABOUT</span>
-							</div>
-							<div className='services'>
-								<span className='span1'>SERVICES</span>
-							</div>
-							<div className='portfolio'>
-								<span className='span1'>PORTFOLIO</span>
-							</div>
-							<img alt='Image' src='../src/img/website-rectangle-1@2x.png' className='rectangle2' />
-							<div className='contactus'>CONTACT US</div>
-							<img alt='Image' src='./img/website-group.png' className='group' />
-							<div className='wecreatecontentth'>
-								We Create
-								<br />
-								Content that
-								<br />
-								ranks
-							</div>
-							<div className='aboutus'>About Us</div>
-							<div className='loremipsumdolorsi'>
-								4ITS focuses on technology outsourcing and IT solutions. We leverage on the team's
-								decades long experience in the areas of IT departments, coding as well as running
-								business organizations and projects.
-							</div>
-							<Service />
-							<div className='rectangle3'></div>
-							<div className='ourvaluableservice'>Our Valuable Services</div>
-							{/* <div className='rectangle4'></div> */}
-							<div className='rectanglecopy3'></div>
-							<div className='rectanglecopy4'></div>
-							{/* <img alt='Image' src='./img/website-bitmap.png' className='bitmap' /> */}
-							<div className='loremipsumdolorsi1'>
-								Web dev dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-								incididunt ut labore et dolore magna aliqua.
-							</div>
-							<img alt='Image' src='./img/website-bitmap-2@2x.png' className='bitmap2' />
-							{/* <div className='loremipsumdolorsicopy'>
-								App dev ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-								incididunt ut labore et dolore magna aliqua.
-							</div> */}
-							<img alt='Image' src='./img/website-bitmap-1.png' className='bitmap1' />
-							<div className='loremipsumdolorsicopy2'>
-								UX/UI ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-								incididunt ut labore et dolore magna aliqua.
-							</div>
-							<div className='webdevelopment'>Web Development</div>
-							{/* <div className='appdevelopment'>App Development</div> */}
-							<div className='uiuxdesign'>UI UX Design</div>
-							<img
-								alt='Image'
-								src='./img/website-rectangle-copy-2@2x.png'
-								className='rectanglecopy5'
-							/>
-							<div className='group2'>
-								<img
-									alt='Image'
-									src='./img/website-rectangle-copy-2@2x.png'
-									className='rectanglecopy6'
-								/>
-								<div className='readmorecopy'>READ MORE</div>
-							</div>
-							<div className='group2copy'>
-								<img
-									alt='Image'
-									src='./img/website-rectangle-copy-2@2x.png'
-									className='rectanglecopy6'
-								/>
-								<div className='seeallwork'>SEE ALL WORK</div>
-							</div>
-							<img
-								alt='Image'
-								src='./img/website-rectangle-copy-2@2x.png'
-								className='rectanglecopy7'
-							/>
-							<div className='readmore'>READ MORE</div>
-							<div className='readmorecopy2'>READ MORE</div>
-							{/* <img alt='Image' src='./img/website-oval-1@2x.png' className='oval1' />
-							<img alt='Image' src='./img/website-path-2@2x.png' className='path2' /> */}
-							<div className='ourwork'>Our Work</div>
-							<div className='brandname'>Brand Name</div>
-							<div className='brandnamecopy2'>Brand Name</div>
-							<div className='brandnamecopy'>Brand Name</div>
-							<div className='brandnamecopy3'>Brand Name</div>
-							<div className='iosandroidweb'>iOS | ANDROID | WEB DEVELOPMENT</div>
-							<div className='iosandroidwebcopy2'>iOS | ANDROID | WEB DEVELOPMENT</div>
-							<div className='iosandroidwebcopy'>iOS | ANDROID | WEB DEVELOPMENT</div>
-							<div className='iosandroidwebcopy3'>iOS | ANDROID | WEB DEVELOPMENT</div>
-							<div className='loremipsumdolorsicopy3'>
-								App Dev r sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-								labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-								ullamco laboris nisi ut aliquip ex ea commodo consequat.
-							</div>
-							<div className='loremipsumdolorsicopy5'>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-							</div>
-							<div className='loremipsumdolorsicopy4'>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-							</div>
-							<div className='loremipsumdolorsicopy6'>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-							</div>
-							<img alt='Image' src='./img/website-line-copy-2@2x.png' className='line' />
-							<img alt='Image' src='./img/website-line-copy-2@2x.png' className='linecopy' />
-							<img alt='Image' src='./img/website-line-copy-2@2x.png' className='linecopy2' />
-							<div className='bitmap3'>
-								<div className='mask'></div>
-								<img alt='Image' src='./img/website-bitmap-3.png' className='bitmap4' />
-							</div>
-							<div className='bitmapcopy2'>
-								<div className='mask'></div>
-								<img alt='Image' src='./img/website-bitmap-4.png' className='bitmap4' />
-							</div>
-							<div className='bitmapcopy'>
-								<div className='mask'></div>
-								<img alt='Image' src='./img/website-bitmap-5.png' className='bitmap4' />
-							</div>
-							<div className='bitmapcopy3'>
-								<div className='mask'></div>
-								<img alt='Image' src='./img/website-bitmap-6.png' className='bitmap4' />
-							</div>
-							<div className='form'>
-								<div className='button'>
-									<img alt='Image' src='./img/website-rectangle-3.png' className='rectangle31' />
-									<div className='sendamessage'>Send a Message</div>
-								</div>
-								<div className='message'>
-									<div className='editbox'>
-										<img alt='Image' src='./img/website-base.png' className='base' />
-										<div className='yourmessagestarts'>Your message starts with…</div>
-									</div>
-									<div className='message1'>Message</div>
-								</div>
-								<div className='subject'>
-									<div className='editbox'>
-										<img alt='Image' src='./img/website-base-2.png' className='base' />
-										<div className='questionaboutyour'>Question about your article</div>
-									</div>
-									<div className='subject1'>Subject</div>
-								</div>
-								<div className='email'>
-									<div className='editbox'>
-										<img alt='Image' src='./img/website-base-2.png' className='base' />
-										<div className='somethingwebsiteco'>something@website.com</div>
-									</div>
-									<div className='youremailaddress'>Your Email Address</div>
-								</div>
-							</div>
-							<div className='questionscomments'>Questions, Comments, Concerns?</div>
-							<div className='weu2019dlovetohearfr'>
-								<span className='span1'>We’d love to </span>
-								<span className='span2'>hear</span>
-								<span className='span3'> from you!</span>
-							</div>
-							<div className='rectangle5'></div>
-							<div className='a2020251allrightsre'>
-								2020 © All rights reserved by Your company name
-							</div>
-						</div>
-					</div>
+			<div className='app'>
+				<Header />
+				<div className='main'>
+					<Main />
+				</div>
+				<div className='services'>
+					<Service />
+					<Service />
+					<Service />
 				</div>
 			</div>
 		);
