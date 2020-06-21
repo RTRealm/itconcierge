8db374de993018a9140cc54ef4e4b56f0da0ba26
Tomasz Gasienica-Szostak <casprovy.tgs@gmail.com>
2020-06-21 11:06:10 +0200

    responsive on maxwidth 850.

--- src/components/Offer/Offer.js
+++ src/components/Offer/Offer.js
@@ -5,25 +5,38 @@ export default function Offer(props) {
 	const rightStyle = {
 		textAlign: 'right',
 	};
-	if (props.alignimage === 'left') {
-		return (
-			<div className='offerbody'>
-				<img alt='offer' src={props.offerimage}></img>
-				<div>
-					<p className='offerBodyTitle'>{props.offertitle}</p>
-					<p className='offerBodyText'>{props.offertext}</p>
+	const screenWidth = window.innerWidth;
+	console.log(screenWidth);
+	if (screenWidth > 850) {
+		if (props.alignimage === 'left') {
+			return (
+				<div className='offerbody'>
+					<img alt='offer' src={props.offerimage}></img>
+					<div>
+						<p className='offerBodyTitle'>{props.offertitle}</p>
+						<p className='offerBodyText'>{props.offertext}</p>
+					</div>
 				</div>
-			</div>
-		);
-	} else {
-		return (
-			<div className='offerbody'>
-				<div style={rightStyle}>
-					<p className='offerBodyTitle'>{props.offertitle}</p>
-					<p className='offerBodyText'>{props.offertext}</p>
+			);
+		} else {
+			return (
+				<div className='offerbody'>
+					<div style={rightStyle}>
+						<p className='offerBodyTitle'>{props.offertitle}</p>
+						<p className='offerBodyText'>{props.offertext}</p>
+					</div>
+					<img alt='offer' src={props.offerimage}></img>
 				</div>
-				<img alt='offer' src={props.offerimage}></img>
-			</div>
-		);
+			);
+		}
 	}
+	return (
+		<div className='offerbody'>
+			<div>
+				<p className='offerBodyTitle'>{props.offertitle}</p>
+				<p className='offerBodyText'>{props.offertext}</p>
+			</div>
+			<img alt='offer' src={props.offerimage}></img>
+		</div>
+	);
 }
