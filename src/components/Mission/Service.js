35295178a3b1ad23da48521ac684777863aa1713
Tomasz Gasienica-Szostak <casprovy.tgs@gmail.com>
2020-07-09 15:56:38 +0200

    copywriting v2

--- src/components/Mission/Service.js
+++ src/components/Mission/Service.js
@@ -11,6 +11,12 @@ export default function Service(props) {
 			<div className='bodyText'>
 				<p>{props.bodylorem}</p>
 			</div>
+			{/* <div>
+				<img
+					alt='4IT logo'
+					src='https://res.cloudinary.com/ohcash/image/upload/v1594239068/itconcierge/logo4.svg'
+				/>
+			</div> */}
 		</div>
 	);
 }
